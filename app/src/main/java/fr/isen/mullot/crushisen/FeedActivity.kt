807027d828cbc8cd43f488377a26124d47b2ec89
Jean-Baptiste Jail <jean-baptite.jail@isen.yncrea.fr>
2024-03-26 22:50:27 +0100

    Card --> feed

--- app/src/main/java/fr/isen/mullot/crushisen/FeedActivity.kt
+++ app/src/main/java/fr/isen/mullot/crushisen/FeedActivity.kt
@@ -1,60 +1,82 @@
 package fr.isen.mullot.crushisen
 
-import android.content.ContentValues.TAG
-import android.content.Context
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.TextField
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.IconToggleButton
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.google.firebase.Firebase
-import com.google.firebase.FirebaseApp
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.firestore
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.GenericTypeIndicator
+import com.google.firebase.database.ValueEventListener
 import fr.isen.mullot.crushisen.ui.theme.CrushIsenTheme
 import kotlinx.coroutines.launch
+
 
 class FeedActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,11 +100,28 @@ fun FeedActivityContent() {
         }
         composable("profile") {
             ProfileEditScreen(navController)
+
         }
         composable("notification") {
             NotificationEditScreen(navController)
         }
 
+    }
+}
+@Composable
+fun NotificationEditScreen(navController: NavHostController){
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            MainHeader(userName = "YourUserName")
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            BottomNavBar(navController = navController)
+        }
     }
 }
 
@@ -211,7 +250,7 @@ class FeedViewModel : ViewModel() {
     }
 }
 
-
+@SuppressLint("RememberReturnType")
 @Composable
 fun ProfileEditScreen(navController: NavHostController) {
     val snackbarHostState = remember { SnackbarHostState() }
@@ -387,136 +426,155 @@ fun ProfileEditScreen(navController: NavHostController) {
                 Text("Mettre à jour")
             }
         }
+
     }
 }
 
-
-
-
-
-
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun FeedEditScreen(navController: NavHostController){
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            MainHeader(userName = "YourUserName")
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            BottomNavBar(navController = navController)
-        }
-    }
-}
-@Composable
-fun NotificationEditScreen(navController: NavHostController){
-
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            MainHeader(userName = "YourUserName")
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            BottomNavBar(navController = navController)
-        }
-    }
-}
-
-
-
-
-/* package fr.isen.mullot.crushisen
-
-import android.content.Context
-import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModel
-import com.google.firebase.FirebaseApp
-import com.google.firebase.database.*
-import fr.isen.mullot.crushisen.ui.theme.CrushIsenTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            CrushIsenTheme {
-                val viewModel = remember { MainViewModel() }
-                val selectedItem = remember { mutableStateOf("Home") }
-
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+fun FeedEditScreen(navController: NavHostController) {
+    CrushIsenTheme {
+        Scaffold(
+            bottomBar = {
+                BottomNavBar(navController = navController)
+            },
+            topBar = {
+                FeedHeader(userName = "Lost")
+            },
+            content = { paddingValues ->
+                // Utiliser paddingValues pour respecter le padding requis par topBar et bottomBar
+                androidx.compose.material.Surface(
+                    Modifier
+                        .padding(paddingValues) // Appliquer le padding global fourni par le Scaffold
+                        .padding(horizontal = 8.dp) // Vous pouvez toujours ajouter vos propres paddings en plus
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color(0xFFF58529), // Orange
+                                    Color(0xFFDD2A7B), // Pink
+                                    Color(0xFF8134AF), // Purple
+                                    Color(0xFF515BD4)  // Blue
+                                )
+                            )
+                        ),
                 ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Text(text = viewModel.message?.entries?.joinToString(", ") { "${it.key}: ${it.value}" } ?: "")
-                        Spacer(modifier = Modifier.weight(1f))
-                        Button(onClick = { viewModel.sendMessage("key2", "Hello, Firebase!") }) {
-                            Text("Send Message")
-                        }
-                        BottomNavBar(selectedItem)
-                    }
+                    StyleCard(
+                        imageResList = listOf(
+                            R.drawable.test_card,
+                            R.drawable.test_card2,
+                            R.drawable.test_card3
+                        ),
+                        username = "Crushisen" // Nom d'utilisateur à afficher
+                    )
+                }
+            }
+        )
+    }
 
-                    LaunchedEffect(true) {
-                        viewModel.fetchData()
+
+
+
+   @OptIn(ExperimentalPagerApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun StyleCard(
+    imageResList: List<Int>, // Liste des ID de ressources d'images
+    username: String, // Nom d'utilisateur à afficher
+) {
+    var liked by remember { mutableStateOf(false) } // État pour gérer si le post est aimé ou non
+    var likesCount by remember { mutableStateOf(0) } // État pour gérer le nombre de likes
+    var showDialog by remember { mutableStateOf(false) } // État pour gérer l'affichage de la Dialog
+
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState()) // Utilisez Column avec verticalScroll ici
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.icon_pp),
+                    contentDescription = "User Icon",
+                    modifier = Modifier
+                        .size(36.dp) // Increase the size here
+                        .padding(8.dp)
+                )
+                Text(
+                    text = username,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+            val pagerState = rememberPagerState(pageCount = { imageResList.size })
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .height(100.dp) // Réduisez la hauteur ici
+                    .clickable { showDialog = true }) { page ->
+                Image(
+                    painter = painterResource(id = imageResList[page]),
+                    contentDescription = "Post Image",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Fit
+                )
+            }
+            Spacer(Modifier.height(4.dp))
+            Image(
+                painter = painterResource(id = R.drawable.icon_comment),
+                contentDescription = "Comment Icon",
+                modifier = Modifier
+                    .size(39.dp) // Set the size to be the same as the heart icon
+                    .padding(8.dp)
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                IconToggleButton(
+                    checked = liked,
+                    onCheckedChange = {
+                        liked = it
+                        if (it) likesCount++ else if (likesCount > 0) likesCount--
                     }
+                ) {
+                    Icon(
+                        imageVector = if (liked) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+                        contentDescription = "Like",
+                        tint = if (liked) Color.Red else Color.Gray
+                    )
+                }
+                Text(text = "$likesCount") // Texte qui affiche le nombre de likes
+                // Ajoutez plus d'éléments ici si nécessaire
+            }
+        }
+    }
+
+    if (showDialog) {
+        Dialog(onDismissRequest = { showDialog = false }) {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .clickable { showDialog = false }) {
+                val pagerState = rememberPagerState(pageCount = { imageResList.size })
+                HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { page ->
+                    Image(
+                        painter = painterResource(id = imageResList[page]),
+                        contentDescription = "Post Image",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Fit
+                    )
                 }
             }
         }
     }
 }
 
-class MainViewModel : ViewModel() {
-    // Message to display
-    var message by mutableStateOf<Map<String, String>?>(null)
-        private set // allows modification only within the class
 
-    // Reference to the Firebase database
-    private val database: DatabaseReference = FirebaseDatabase.getInstance().reference
 
-    // Fetch data from the Firebase database
-    fun fetchData() {
-        database.child("test").addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val typeIndicator = object : GenericTypeIndicator<HashMap<String, String>>() {}
-                val value = dataSnapshot.getValue(typeIndicator)
-                message = value ?: mapOf("No message" to "found")
-            }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.e("Firebase", "Error fetching message: ${databaseError.message}")
-                message = mapOf("Error fetching message" to "")
-            }
-        })
-    }
 
-    // Send a message to Firebase
-    fun sendMessage(key: String, value: String) {
-        database.child("test").child(key).setValue(value)
-    }
 }
-
-@Composable
-fun GreetingPreview() {
-    CrushIsenTheme {
-        Text("Android")
-    }
-}
- */
